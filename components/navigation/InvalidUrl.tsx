400c9f232c79704d67347ce05c4cab69abe1a433
Ignacio <rogerpeke97@gmail.com>
2022-02-25 22:19:24 -0300

    Added tailwind config, and finished invalid url page

--- components/navigation/InvalidUrl.tsx
+++ components/navigation/InvalidUrl.tsx
@@ -1,12 +1,16 @@
-import { faRadiation } from "@fortawesome/free-solid-svg-icons"
+import { faRadiation, faBug, faExclamation } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { useContext } from "react"
+import { AppContextHelpers } from "../../context/AppContextHelpers"
 
 
-const invalidUrl = () => {
+const InvalidUrl = () => {
+
+  const { windowWidth } = useContext(AppContextHelpers)
 
   return (
-    <div className="h-screen pt-16 flex justify-center w-full">
-      <div className="flex flex-col">
+    <div className={`h-full flex w-full ${windowWidth.description === 'small' ? 'flex-col' : 'pt-40'}`}>
+      <div className="flex pt-24 xs:pt-0 pb-14 w-full flex-col">
         <div className="flex items-center justify-center">
           <FontAwesomeIcon className="icon text-card" icon={faRadiation} />
           <h1 className="px-4 heading-2 font-bold text-center">
@@ -14,15 +18,29 @@ const invalidUrl = () => {
           </h1>
           <FontAwesomeIcon className="icon text-card" icon={faRadiation} />
         </div>
-        <h3 className="pt-5 subtitle-3 font-semibold">
-          The url you are trying to access is not valid.
-          <br />
-          Please pick a valid menu from the side bar.
-        </h3>
+        <div className="flex items-center justify-center">
+          <h3 className="pt-5 subtitle-3 font-semibold">
+            The url you are trying to access is not valid.
+            <br />
+            Please pick a valid menu from the side bar.
+          </h3>
+        </div>
+      </div>
+      <div className="w-full flex justify-center">
+        <div className="w-96 p-12 rounded-lg flex flex-col bg-background items-center">
+          <div className="flex items-center">
+            <h1 className="px-4 heading-1 font-bold text-card text-center">
+              404
+            </h1>
+          </div>
+          <div className="flex pt-10 items-center">
+            <FontAwesomeIcon className="icon-big text-card" icon={faExclamation} />
+          </div>
+        </div>
       </div>
     </div>
   )
 }
 
 
-export default invalidUrl
+export default InvalidUrl
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,25 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-    colors: {
-      "primary": "#020A33",
-      "background": "#1e2429",
-      "button": {
-        light:  "#E9ECEF",
-        dark:   "#020A33",
+    extend: {
+      colors: {
+        "primary": "#020A33",
+        "background": "#1e2429",
+        "button": {
+          light:  "#E9ECEF",
+          dark:   "#020A33",
+        },
+        "card": "#0070f3"
       },
-      "card": "#0070f3"
-    }
+      screens: {
+        'xs': {min: '320px', max: '640px'},
+        'sm': {min: '640px', max: '768px'},  
+        'md': {min: '768px', max: '1024px'},  
+        'lg': {min: '1024px', max: '1280px'},  
+        'xl': {min: '1280px', max: '1536px'},  
+        '2xl': '1536px',
+      }
+    },
   },
   plugins: [],
 }
